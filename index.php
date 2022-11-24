ffab427ef31993d4a1525d7208a3105525f4a31e
saadilina <118968244+saadilina@users.noreply.github.com>
2022-11-24 10:13:00 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php 
+    include("includes/header.php")
+?>
+    <div class="container-fluid py-4">
+      <div class="row min-vh-80 h-100">
+        <div class="col-12">
+            hello 
+        </div>
+      </div>
+    </div>
+        
+       
+
+<?php 
+    include("includes/footer.php")
+?>
